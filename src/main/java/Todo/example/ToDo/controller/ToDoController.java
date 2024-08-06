075f9f7fe4bd453dac6f19c28d51c466de55f89b
Kushala C L <103378341+its-kushala@users.noreply.github.com>
2024-08-06 21:04:45 +0530

    Another commit

--- src/main/java/Todo/example/ToDo/controller/ToDoController.java
+++ src/main/java/Todo/example/ToDo/controller/ToDoController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import Todo.example.ToDo.App.ToDoService;
+import Todo.example.ToDo.App.model.ToDo;
 
 @Controller
 public class ToDoController {
@@ -16,10 +18,10 @@ public class ToDoController {
 	@Autowired
 	private ToDoService service;
 	
-	@GetMapping(("/", "viewToDoList"))  
+	@GetMapping("/viewToDoList")  
 	public String viewAllToDoItems(Model model, @ModelAttribute("message") String message) {
-		model.addAllAttributes("list", service.getAllToDoItems());
-		model.addAllAttributes("msg", message);
+		model.addAttribute("list", service.getAllToDoItems());
+		model.addAttribute("msg", message);
 		
 		return "ViewToDoList";
 		
@@ -44,7 +46,7 @@ public class ToDoController {
 	
 	@PostMapping("/saveToDoItem")
 	public String saveToDoItem(ToDo todo, RedirectAttributes redirectAttributes) {
-		if (services.saveOrUpdateToDoItem(todo)) {
+		if (service.saveOrUpdateToDoItem(todo)) {
 			redirectAttributes.addFlashAttribute("message", "Save Success");
 			return "redirect:/viewToDoList";
 		}
@@ -53,7 +55,7 @@ public class ToDoController {
 	}
 	
 	@GetMapping("/editToDoItem/{id}")
-	public String editToDoItem(@PathVariable Long id) {
+	public String editToDoItem(@PathVariable Long id, Model model) {
 		model.addAttribute("todo", service.getToDoItemById(id));
 		
 		return "EditToDoItem";
@@ -73,9 +75,9 @@ public class ToDoController {
 	@GetMapping("/deleteToDoItem/{id}")
 	public String deleteToDoItem(@PathVariable Long id, RedirectAttributes redirectAttributes) {
 		if(service.deleteToDoItem(id)) {
-			redirctAttributes.addFlashAttribute("message", "Delete Success");
+			redirectAttributes.addFlashAttribute("message", "Delete Success");
 		}
-		redirctAttributes.addFlashAttribute("message", "Delete Failure");
-		return "
+		redirectAttributes.addFlashAttribute("message", "Delete Failure");
+		return "redirect:/viewToDoList";
 	}
 }
